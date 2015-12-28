4e058dbac96d5b6d2f8c9ccb4f280a8d4b9b5001
canduecho <179438678@qq.com>
2015-12-28 16:38:34 +0800

    test

--- myapitest/Global.asax.cs
+++ myapitest/Global.asax.cs
@@ -14,6 +14,7 @@ namespace myapitest
 
     public class MvcApplication : System.Web.HttpApplication
     {
+
         public static void RegisterGlobalFilters(GlobalFilterCollection filters)
         {
             filters.Add(new HandleErrorAttribute());
